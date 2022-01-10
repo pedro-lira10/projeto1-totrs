c6ff00f9cdad2c059796590e56651692aefd18cb
Pedro Lira <56555559+pedro-lira10@users.noreply.github.com>
2022-01-10 19:14:06 -0300

    new

---
+++ exercicios js/historia de confla 1/Bucles e iteración/script.js
@@ -0,0 +1,33 @@
+//  while
+//let num = 0;
+
+//while (num <= 9) {
+    
+//    num++;
+//    document.write( num + "<br>")
+////
+//}
+
+
+// # break
+//let numero = -2;
+
+//while(numero < 30) {
+    //numero++;
+    //document.write(numero);
+//    if (numero == 5) {
+//        break;
+//    }
+    
+//    }
+
+//document.write('fin')
+
+
+// # do .. while
+
+let numero = 0;
+
+for (numero<15) {
+    numero++;
+}
---
+++ exercicios js/historia de confla 1/script.js
@@ -0,0 +1,3 @@
+dinercofla = prompt("quanto de dinheiro você possue?")
+dinercoRoberto = prompt("quanto de dinheiro você possue?")
+dinercoPedro = prompt("quanto de dinheiro você possue?")
