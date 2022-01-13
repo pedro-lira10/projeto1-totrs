37237f249135d61e9f8be6967d25476aeb6a33ad
Pedro Lira <56555559+pedro-lira10@users.noreply.github.com>
2022-01-13 01:46:53 -0300

    new

---
+++ exercicios js/historia de confla 1/function/script.js
@@ -0,0 +1,85 @@
+// # function - Definicão e uso
+//function saludar() {
+  //  respuesta = prompt("!hola titan, ?como due tu día?");
+    //if (respuesta == "bien") {
+      //  alert("me alegro")
+    //}else {
+      //  alerta("una pena")
+    //}
+//}
+
+//saludar()
+
+//----------------------------------
+
+
+//function saludar() {
+    //return "todo okay";
+//    alert("hola")
+//}
+
+//let saludo = saludo();
+
+//document.write(saludo)
+
+//let num1 = 32;
+//let num2 = 12;
+
+//function suma(){
+//    let res = num1 + num2;
+//    document.write(res);
+//}
+
+//suma()
+
+//----
+
+//function suma(num1,num2){
+//    let res = num1 + num2;
+//    document.write(res);
+//    document.write("<br>")
+//}
+
+//suma(12,35)
+
+//----
+
+//function suma(num1,num2){
+//   let res = num1 + num2;
+//  return res
+//}
+
+//let resultado = suma(2,88);
+
+//document.write(resultado);
+//document.write("<br>")
+
+
+
+
+//---------------------------------------------
+
+
+// # function - Parametros
+
+//function saludar (nombre) {
+//    let frase = `!hola ${nombre} ! ? como está?`;
+//    document.write(frase)
+//}
+
+//saludar("kratos")
+
+//-------------------------------------------
+
+// # function - scope
+
+
+
+
+
+// - formas de criar funções ( e chamar)
+// - return
+// - Parametros
+// - scope
+// - funções flecha
+
