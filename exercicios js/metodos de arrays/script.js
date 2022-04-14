51e29343762217ed8b09fb673352b0167167f967
Pedro Lira <56555559+pedro-lira10@users.noreply.github.com>
2022-04-13 21:57:44 -0300

    tg

---
+++ exercicios js/metodos de arrays/script.js
@@ -0,0 +1,7 @@
+let numeros = ["abecedario","manzana","pedro","dedo","bobo"];
+
+document.write(numeros +"<br>");
+let resultado = numeros.slice(0,-1);
+document.write(resultado);
+
+console.log(resultado)
