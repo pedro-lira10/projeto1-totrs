7831cec5f0fd87f4376376b22fc5bd2e22138b32
Pedro Lira <56555559+pedro-lira10@users.noreply.github.com>
2022-04-16 16:08:16 -0300

    new

--- exercicios js/metodos de arrays/script.js
+++ exercicios js/metodos de arrays/script.js
@@ -1,7 +1,16 @@
 let nombres = ["abecedario","manzana","pedro","dedo","bobo"];
 
 document.write(nombres + "<br>");
-let resultado = nombres.push("kalel","magnatrom");
+let resultado = nombres.reverse("kalel","magnatrom");
+
+
 
 document.write(nombres);
 
+let number = [7,8,7,9,1,2,5,4,5,5];
+
+
+
+document.write(number + "<br>")
+number.sort()
+document.write(number)
