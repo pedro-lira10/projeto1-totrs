2d4eaecb3aacaa14a28a3d2ab50a4c0a0e006a26
Pedro Lira <56555559+pedro-lira10@users.noreply.github.com>
2022-04-16 16:09:21 -0300

    l

--- exercicios js/metodos de arrays/script.js
+++ exercicios js/metodos de arrays/script.js
@@ -1,7 +1,7 @@
 let nombres = ["abecedario","manzana","pedro","dedo","bobo"];
 
 document.write(nombres + "<br>");
-let resultado = nombres.spli("kalel","magnatrom");
+let resultado = nombres.splic("kalel","magnatrom");
 
 
 
