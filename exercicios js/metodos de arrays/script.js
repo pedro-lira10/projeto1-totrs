771496f0d054951df97c8e54ed93e4b5bbb51535
Pedro Lira <56555559+pedro-lira10@users.noreply.github.com>
2022-04-17 08:47:06 -0300

    Update script.js

--- exercicios js/metodos de arrays/script.js
+++ exercicios js/metodos de arrays/script.js
@@ -1,2 +1,7 @@
 let numeros = ["abecedario","manzana","pedro","dedo","bobo","peter"];
-//
+// let cadena2 = "";
+
+resultado = numeros.filter(numero =>numeros.length > 5)
+
+document.write(resultado)
+
