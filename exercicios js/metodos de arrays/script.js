1d5a8a33b65843ddcfa719fb80a5a45be5a8959e
Pedro Lira <56555559+pedro-lira10@users.noreply.github.com>
2022-04-18 20:04:12 -0300

    new

--- exercicios js/metodos de arrays/script.js
+++ exercicios js/metodos de arrays/script.js
@@ -1,7 +1,61 @@
-let numeros = ["abecedario","manzana","pedro","dedo","bobo","peter","otimusprime","kim","kalif","whi"];
-// let cadena2 = "";
+class Celular {
+	constructor(color,peso,tamaño,rdc,ram){
+		this.color = color;
+		this.peso = peso;
+		this.tamaño = tamaño;
+		this.resolucionDeCamara = rdc;
+		this.memoriaRam = ram;
+		this.encendido = false;
+	}
+	presionarBotonEncendido(){
+		if (this.encendido == false) {
+			alert("celular prendido");
+			this.encendido = true;
+		} else {
+			alert("celular apagado");
+			this.encendido = false;
+		}
+	}
 
-resultado = numeros.filter(numero => numero.length > 4)
+	reiniciar(){
+		if (this.encendido == true) {
+			alert("reiniciando celular");
+		} else {
+			alert("el celular está apagado");
+		}
+	}
+	tomarFoto(){
+		alert(`foto tomada en una resolución de: ${this.resolucionDeCamara}`);
+	}
+	grabarVideo(){
+		alert(`grabando video en ${this.resolucionDeCamara}`);
+	}
+	mobileInfo(){
+		return `
+		Color: <b>${this.color}</b></br>
+		Peso: <b>${this.peso}</b></br>
+		Tamaño: <b>${this.tamaño}</b></br>
+		Resolución de Video: <b>${this.resolucionDeCamara}</b></br>
+		Memoria Ram: <b>${this.memoriaRam}</b></br>
+		`;
+	}
+}
 
-document.write(resultado)
 
+celular1 = new Celular("rojo","200g","5'","hd","1GB");
+celular2 = new Celular("negro","155g","5.4'","full hd","2GB");
+celular3 = new Celular("blanco","15046g","5.9'","full hd","2GB");
+
+// celular1.presionarBotonEncendido();
+// celular1.tomarFoto();
+// celular1.grabarVideo();
+// celular1.reiniciar();
+// celular1.presionarBotonEncendido();
+
+
+
+document.write(`
+	${celular1.mobileInfo()} <br>
+	${celular2.mobileInfo()} <br>
+	${celular3.mobileInfo()} <br>
+	`);
