5c71d88ecfce5c4dd431a93b9e1410a08c18e138
Pedro Lira <56555559+pedro-lira10@users.noreply.github.com>
2022-04-26 04:43:44 -0300

    new

---
+++ exercicios js/javascript/Brazilian Dev/script.js
@@ -0,0 +1,10 @@
+const dotNotation = {
+    prop1 : " sou uma propriedade que será acessada usando dot motation",
+    metodo: function () {
+        return "sou o retorno de um método,ou seja, uma função dentro de um objeto."
+    }
+}
+
+console.log (dotNotation.prop1 )
+console.log (dotNotation.metodo )
+ 
