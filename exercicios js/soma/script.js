aae0bfcb0bcca437535a8ed47de24b875407e633
pedro <pedroiago510@yahoo.com>
2023-02-21 20:15:07 -0300

    aqui no vs code

--- exercicios js/soma/script.js
+++ exercicios js/soma/script.js
@@ -2,6 +2,10 @@ const soma = (a, b) => a + b
 
 const multiplica = (a, b) => a * b
 
+const divi = (a, b) => a / b
+
 console.log(soma(2, 3))
 
 console.log(multiplica(2, 1))
+
+console.log(divi(2, 3))
